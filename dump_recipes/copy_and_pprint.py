64561510c1feff82055a759f2b4a65153d35fc1e
jforr <John.forr@gmail.com>
2016-12-09 15:41:05 -0500

    Update copy_and_pprint.py

--- dump_recipes/copy_and_pprint.py
+++ dump_recipes/copy_and_pprint.py
@@ -9,16 +9,24 @@ import os
 SCRIPT_OUTPUT = os.path.join(
   os.environ['APPDATA'],
   'Factorio',
-  'script-output',
-  'recipes'
+  'script-output'
 )
 
-def main():
-    with open(SCRIPT_OUTPUT) as fp:
+files = ('recipes','technologies')
+
+def parse_files(file_type):
+    file_path = os.path.join(SCRIPT_OUTPUT,file_type)
+    with open(file_path) as fp:
         obj = json.load(fp)
+
+    output_file = file_type + '.json'
         
-    with open('recipes.json', 'w') as out:
+    with open(output_file, 'w') as out:
         json.dump(obj, out, indent=1, sort_keys=True)
+
+def main():
+    for i in files:
+        parse_files(i)
 
 if __name__ == '__main__':
     main()
